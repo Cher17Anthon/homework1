9661a52f981e418db0be9305e9d0ee94287a401c
Cherepkov Anton <120743916+Cher17Anthon@users.noreply.github.com>
2025-08-19 23:01:43 +0300

    Revert "Homework 4"

--- src/features/PostLengthFilter/lib/filterByLength.ts
+++
@@ -1,5 +0,0 @@
-type Post = { id: number; title: string; body: string };
-
-export const filterByLength = (posts: Post[], minLength: number) => {
-  return posts.filter(post => post.title.length >= minLength);
-};
--- src/widgets/LayoutFooter/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>2025</p>
-    </footer>
-  );
-};
-
-export default Footer;
